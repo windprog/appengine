87bf0a3a9212c4ab5bee859ac16340e74be31720
windpro <windprog@gmail.com>
2014-07-23 01:45:22 +0800

    no message

--- engine/scheduler.py
+++ engine/scheduler.py
@@ -18,13 +18,8 @@ class Scheduler(object):
 
     __slots__ = ("_engine", "_handler")
 
-<<<<<<< HEAD
-    async = set()  # 异步标志
-    last = defaultdict(lambda: 0)  # 最后调度时间
-=======
     async = dict()  # 异步标志
     last = defaultdict(lambda: 0)  # 最后采样时间
->>>>>>> dc8d9e94d8ee92500f4b1141971930fa5bd3e299
     samples = defaultdict(tuple)  # 采样列表
 
     def __init__(self, engine, handler):
@@ -32,30 +27,6 @@ class Scheduler(object):
         self._handler = handler
 
     def __enter__(self):
-<<<<<<< HEAD
-        execute = self._handler in self.async and \
-            partial(self._engine.async_execute, self._handler) or \
-            self._handler
-
-        self._start = time()
-        return execute
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        if self._start - self.last[self._handler] >= 60:  # 1 分钟
-            # 本次执行是否超出阈值。
-            exceed = (time() - self._start) >= THRESHOLD
-
-            # 最后的采样列表。
-            samples = self.samples[self._handler]
-            samples = len(samples) >= 6 and samples[-5:] + (exceed,) or samples + (exceed,)
-
-            # 更新状态。
-            all(samples) and self.async.add(self._handler) or self.async.discard(self._handler)
-            self.samples[self._handler] = samples
-            self.last[self._handler] = self._start
-
-        return True  # 阻断异常
-=======
         return time() - self.last[self._handler] >= 60 and \
             self._sample or \
             self.async.get(self._handler, self._handler)
@@ -89,4 +60,3 @@ class Scheduler(object):
         self.last[self._handler] = time()
 
         return ret
->>>>>>> dc8d9e94d8ee92500f4b1141971930fa5bd3e299
