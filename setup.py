17e84bc08650ffc59ef08601be62ed457a9705e0
windpro <windprog@gmail.com>
2014-12-02 23:21:55 +0800

    第一个可用版本：version:0.0.1

--- setup.py
+++ setup.py
@@ -21,6 +21,9 @@ setup(
     url="https://github.com/windprog/appengine",
     author=__author__,
     author_email="windprog@gmail.com",
-    packages=['appengine'],
+    packages=['appengine', 'appengine.engine', 'appengine.engine.driver'],
+    # package_data = {
+    #     'appengine': ['engine/*.py'],
+    # },
     install_requires=['gevent>=1.0.1', 'Werkzeug>=0.9.6']
 )
