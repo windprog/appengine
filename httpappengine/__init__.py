45bdd3d4468de96abf50386a9f640b925dfc8dd6
windpro <windprog@gmail.com>
2014-12-04 17:02:53 +0800

    更新版本号:0.0.2

--- httpappengine/__init__.py
+++ httpappengine/__init__.py
@@ -9,7 +9,7 @@ E-mail  :   windprog@gmail.com
 Date    :   14-12-2
 Desc    :   
 """
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "Windpro"
 __description__ = "High performance http engine, Support Django."
 __title__ = 'httpappengine'
