a490dbb657f3f8dbc00bdc2e5ecf0854fe38ecdd
windpro <windprog@gmail.com>
2014-12-26 18:34:04 +0800

    更新测试例子，多进程同步示例。

--- action/hello.py
+++ action/hello.py
@@ -2,6 +2,8 @@
 
 from urllib2 import urlopen
 from time import sleep
+import multiprocessing
+
 from httpappengine import url
 from httpappengine.helper import rest
 
@@ -52,22 +54,32 @@ def rest_hello(environ, start_response):
 
     return rest(start_response, data)
 
+
+# 多进程同步例子， 使用共享内存, 类型在fmultiprocessing.sharedctypes.typecode_to_type
+global_select_count = multiprocessing.Value('I', 0)
+
+
 @url("/add", "GET")
 def add(environ, start_response):
-    if not hasattr(add, "test_count"):
-        test_count = 0
-        setattr(add, "test_count", test_count)
-        print "set"
+    # 快速刷新这个页面会有效果
+    if not hasattr(add, "local_select_count"):
+        setattr(add, "local_select_count", 1)
+        print "local_select_count init"
     else:
-        test_count = getattr(add, "test_count")
-        setattr(add, "test_count", test_count+1)
+        setattr(add, "local_select_count", getattr(add, "local_select_count")+1)
+
+    # 获取已自增的test_count引用
+    local_select_count = getattr(add, "local_select_count")
+    # 共享内存变量+1
+    global_select_count.value += 1
 
     from httpappengine.engine.router import Router
     import os
-    Router.instance._selector.add("/test%s" % test_count, ['GET'], add)
+    Router.instance._selector.add("/test%s" % local_select_count, ['GET'], add)
 
-    s = "<a href='/test%s'>test%s, select_count:%s, pid:%s</a>" % (
-        test_count, test_count, len(Router.instance._selector._maps._rules), os.getpid())
+    s = "route_rules_count:{0}, url:<a href='/test{1}'>/test{1}</a>, local_select_count:{1}, global_select_count:{2}, pid:{3}".format(
+        len(Router.instance._selector._maps._rules), local_select_count, global_select_count.value, os.getpid()
+    )
 
     start_response("200 OK", [
         ("Content-Type", "text/html"),
